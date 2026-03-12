fce8f3a8ae7514eb95c8c82e126373e3cda79e12
kattashivabalaji2-ai <kattashivabalaji2@gmail.com>
2026-03-12 09:37:07 +0530

    ASSEESSMENT 3

--- src/MODULE_7/QuickSortExample.java
+++ src/MODULE_7/QuickSortExample.java
@@ -39,7 +39,7 @@ public class QuickSortExample {
     public static void main(String[] args) {
         int[] arr = {10, 7, 8, 9, 1, 5};
 
-        System.out.println("Original Array: " + Arrays.toString(arr));
+        System.out.println("Original Array:" + Arrays.toString(arr));
 
         quickSort(arr, 0, arr.length - 1);
 
